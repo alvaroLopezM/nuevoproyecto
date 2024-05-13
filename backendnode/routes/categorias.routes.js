1efe6bd0a40e782ae8af0a5302a0961dcc40737f
alvaroLopezM <131178547+alvaroLopezM@users.noreply.github.com>
2024-05-13 09:19:53 -0600

    Commit backend en node

---
+++ backendnode/routes/categorias.routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const categorias = require('../controllers/categorias.controller')
+
+//GET api/categorias
+router.get('/', categorias.getAll);
+
+
+//GET api/categorias/5
+router.get('/:id', categorias.get);
+
+
+//POST api/categorias
+router.post('/', categorias.create);
+
+
+//PUT api/categorias/5
+router.put('/:id', categorias.update);
+
+
+//DELETE api/categorias
+router.delete('/:id', categorias.delete);
+
+module.exports = router
